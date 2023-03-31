b07bb2d9c3427d0d1a024f5fcdf0523341e19a9f
Luanbt1 <Luanbt63@gmail.com>
2023-03-31 20:53:32 -0300

    first commit

---
+++ src/components/Filme/index.js
@@ -0,0 +1,76 @@
+import "./Filme.css";
+
+const filmes = [
+  {
+    nome: "It",
+    duracao: "2H30",
+    foto: "filme1.jpg",
+    assistido: true,
+    ano: 2018,
+    genero: "Terror",
+    descricao: "Palhaco assasino",
+    nota: 10,
+  },
+  {
+    nome: "John Wick",
+    duracao: "1H30",
+    foto: "filme2.jpg",
+    assistido: true,
+    ano: 2014,
+    genero: "Ação/Nerd",
+    descricao: "Filme de acao com muita morte",
+    nota: 10,
+  },
+  {
+    nome: "Titanic",
+    duracao: "1H20",
+    foto: "filme3.webp",
+    assistido: false,
+    ano: 2000,
+    genero: "Drama",
+    descricao: "Barco naufragando e tristeza",
+    nota: 7,
+  },
+];
+
+function Assistido({javisto}) {
+  if (javisto) {
+    return <p className='text-success'>Filme já assistido</p>;
+  } else {
+    return <p className='text-danger'>Nao assistido</p>;
+  }
+}
+
+export default function Filme() {
+  return (
+    <div className="container text-center">
+      <div className="row">
+        {filmes.map((filme, i) => (
+          <div className="col" key={i.toString()}>
+            <div className="card">
+              <img
+                src={"/assets/images/" + filme.foto}
+                alt={filme.nome}
+                className="card-img-top"
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  {filme.nome} ({filme.ano}){" "}
+                </h5>
+                <p>Sinopse</p>
+                <p className="card-text">Descricao: {filme.descricao}</p>
+                <p>Duracao: {filme.duracao}</p>
+                <p>Genero: {filme.genero}</p>
+                <p>Nota: {filme.nota}</p>
+                <Assistido javisto = {filme.assistido} />
+                <a href={`/detalhes/${filme.nome}`}>
+                  <div className="btn btn-primary">Detalhes</div>
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/sobre.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Informacoes from '../components/Informacoes';
+import Title from './../components/Title/index'
+
+function Sobre() {
+    return (
+        <div>     
+             <Title title='Sobre nossa empresa' text=''/>    
+             <Informacoes descricao='  Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type 
+                        specimen book. It has survived not only five centuries, but also the leap into
+                         electronic typesetting, remaining essentially unchanged. It was popularised 
+                         in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                         and more recently with desktop
+                         publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                          has been the industrys standard dummy text ever since the 1500s, when an unknown printer 
+                          took a galley of type and scrambled it to make a type specimen book. It has survived not 
+                          only five centuries, but also the leap into electronic typesetting, remaining essentially 
+                          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+                          Lorem Ipsum passages, and
+                          more recently with desktop publishing software like Aldus PageMaker including versions of
+                           Lorem IpsumLorem Ipsum passages, and
+                          more recently with desktop publishing software like Aldus PageMaker including versions of
+                           Lorem IpsumLorem Ipsum passages, and
+                          more recently with desktop publishing software like Aldus PageMaker including versions of
+                           Lorem Ipsum.'/>
+        </div>
+    )
+}
+
+export default Sobre;
